1c957fd6655d3108ed7ae80d5f2dcf373150f01f
Jahdiel Vargas <32346581+jahdielvargas@users.noreply.github.com>
2022-03-09 23:45:32 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function next2() {
+    document.getElementById("p1").classList.add("hide")
+    document.getElementById("p2").classList.remove("hide")
+  }
+  function next3() {
+    document.getElementById("p2").classList.add("hide")
+    document.getElementById("p3").classList.remove("hide")
+  }
+  function next4() {
+    location.href= atob('aHR0cHM6Ly9ob21icmVhbGZhLm9ubGluZS9scC9pbmRleC5odG1s')
+  }
